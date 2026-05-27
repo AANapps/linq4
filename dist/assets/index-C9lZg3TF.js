1c132af403e4aea270a4898cd6bf4f5646b69c36
AANapps <adastranetwork@outlook.com>
2026-05-27 12:22:11 +0000

    Revert "Daily vote: replace neon red/purple with deep indigo, premium feel"
    This reverts commit 3e3e640f84d00c828639916e4e4e2ef828f87e5b.

---
+++ dist/assets/index-C9lZg3TF.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BWTRWvOo.js","assets/index-r_E_uxaZ.js","assets/index-BBg9sJR-.css"])))=>i.map(i=>d[i]);
+import{r as t,_ as e}from"./index-r_E_uxaZ.js";const a=t("CapacitorNfc",{web:()=>e(()=>import("./web-BWTRWvOo.js"),__vite__mapDeps([0,1,2])).then(r=>new r.CapacitorNfcWeb)});export{a as CapacitorNfc};
